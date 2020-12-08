786164a1eba0df0ed71208b6d9e845e6b2cb80d7
BrandonLiang <xl2891@columbia.edu>
2020-12-08 13:36:49 -0500

    added bin/lang/cp_dict_to_data.sh and bin/lang_create_lang_dir.sh to create a lang dir using kaldi scripts

---
+++ bin/lang/cp_dict_to_data.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Note: to use 'greadlink', you need to have coreutils installed on your MacOS ("brew install coreutils")
+#       if you are on Linux, use 'readlink' instead
+
+SCRIPT=`greadlink -f "$0"`
+#SCRIPT=`readlink -f "$0"`
+SCRIPT_DIR=`dirname "$SCRIPT"`
+APP_HOME="$SCRIPT_DIR"/../..
+
+CONF=$APP_HOME/conf
+PYTHON_DIR=$APP_HOME/python
+ENV="$CONF"/env.sh # configuration file
+source "$ENV"
+
+rm -fr $KALDI_DATA_LOCATION/local/*
+mkdir -p $KALDI_DATA_LOCATION/local/
+cp -r $APP_HOME/nsc_dict_data/local/dict $KALDI_DATA_LOCATION/local/
---
+++ bin/lang/path.sh
@@ -0,0 +1,1 @@
+/Users/brandonliang/src/kaldi/egs/tedlium/s5_r3/path.sh
