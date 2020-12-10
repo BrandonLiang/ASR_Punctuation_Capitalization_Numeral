7186acc9bbea0ea3c1a1824acf8407045a163cb1
BrandonLiang <brandonliang@qq.com>
2020-12-10 20:46:34 +0000

    added run_test.sh for both versions

---
+++ bin/run_test.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Note: to use 'greadlink', you need to have coreutils installed on your MacOS ("brew install coreutils")
+#       if you are on Linux, use 'readlink' instead
+
+#SCRIPT=`greadlink -f "$0"`
+SCRIPT=`readlink -f "$0"`
+SCRIPT_DIR=`dirname "$SCRIPT"`
+APP_HOME="$SCRIPT_DIR"/..
+
+CONF=$APP_HOME/conf
+PYTHON_DIR=$APP_HOME/python
+INFERENCE=$APP_HOME/inference
+ENV="$CONF"/env.sh # configuration file
+source "$ENV"
+source $SCRIPT_DIR/path.sh
+source $SCRIPT_DIR/cmd.sh
+
+# Use trained GMM-HMM TDNN RNN LM Model (on NSC) to make predictions on test data
+# Following https://medium.com/@nithinraok_/decoding-an-audio-file-using-a-pre-trained-model-with-kaldi-c1d7d2fe3dc5
+
+# 1. generate ivector files and respective configuration files as required for decoding
+$SCRIPT_DIR/steps/online/nnet3/prepare_online_decoding.sh\
+  --mfcc-config $SCRIPT_DIR/conf/mfcc_hires.conf \
+  $KALDI_DATA_LOCATION/lang_chain \
+  $KALDI_MODEL_LOCATION/nnet3/extractor \
+  $KALDI_MODEL_LOCATION/rnnlm_lstm_tdnn_a_averaged \
+  $KALDI_MODEL_LOCATION/rnnlm_lstm_tdnn_a_averaged_online
+
+# 2. make HCLG.fst graph
+$SCRIPT_DIR/utils/mkgraph.sh \
+  --self-loop-scale 1.0 \
+  $KALDI_DATA_LOCATION/test \
+  $KALDI_MODEL_LOCATION/rnnlm_lstm_tdnn_a_averaged_online \
+  $KALDI_MODEL_LOCATION/rnnlm_lstm_tdnn_a_averaged_online/graph
+
+# 3. generate decoded lattices from the file based on mdl file and graph
+$SCRIPT_DIR/steps/online/nnet3/decode.sh \
+  --nj $NJ \
+  --acwt 1.0 \
+  --post-decode-acwt 10.0 \
+  $KALDI_MODEL_LOCATION/rnnlm_lstm_tdnn_a_averaged_online/graph \
+  $KALDI_DATA_LOCATION/test \
+  $KALDI_MODEL_LOCATION/rnnlm_lstm_tdnn_a_averaged_online/decode_test
+
+# 4. decode the lattices
+mkdir -p $INFERENCE
+$KALDI_ROOT/src/latbin/lattice-best-path ark:'gunzip -c $KALDI_MODEL_LOCATION/rnnlm_lstm_tdnn_a_averaged_online/decode_test/lat.*.gz |' 'ark,t:|int2sym.pl -f 2- $KALDI_MODEL_LOCATION/rnnlm_lstm_tdnn_a_averaged_online/graph/words.txt > $INFERENCE/decoded_text.txt'
---
+++ run_test_v2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Note: to use 'greadlink', you need to have coreutils installed on your MacOS ("brew install coreutils")
+#       if you are on Linux, use 'readlink' instead
+
+#SCRIPT=`greadlink -f "$0"`
+SCRIPT=`readlink -f "$0"`
+SCRIPT_DIR=`dirname "$SCRIPT"`
+APP_HOME="$SCRIPT_DIR"
+
+CONF=$APP_HOME/conf
+PYTHON_DIR=$APP_HOME/python
+BIN_DIR=$APP_HOME/bin_v2
+ENV="$CONF"/env.sh # configuration file
+source "$ENV"
+
+STAGE=1
+
+# Stage 1 - Run Test
+# Following https://medium.com/@nithinraok_/decoding-an-audio-file-using-a-pre-trained-model-with-kaldi-c1d7d2fe3dc5
+if [ $STAGE -le 1 ]; then
+
+  $BIN_DIR/run_test.sh
+
+fi
